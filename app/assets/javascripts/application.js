9311c922f0309b5ec50cba47b663a5025cd73d32
Surmin Dmitry <surmin2005@yandex.ru>
2014-10-01 15:01:19 +0400

    up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require fotorama
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'company', to: 'company#index'
+  get 'jobs', to: 'jobs#index'
 end
