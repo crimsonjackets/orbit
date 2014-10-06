f45211a6658f13ccb4114a7f27b085426a119d46
Surmin Dmitry <surmin2005@yandex.ru>
2014-10-06 19:46:24 +0400

    all

--- app/assets/javascripts/makecarusel.js
+++ app/assets/javascripts/makecarusel.js
@@ -1,98 +1,96 @@
 (function($){
   $.fn.makecarousel = function( params ) {
     var config = {
+      inStep: 1,
       next: null,
       prev: null,
       dots: null,
       currentNum: 0,
-      transition: '.5s all ease'
+      transition: '.5s all ease',
+      cover: false
     };
-
     config = $.extend(config, params);
-
     step = this.width();
-    var w, t, ws, ts, text = [];
-
+    var w, t, ws, ts, text = [], u;
     mcMakeBody = function(_this) {
       _this.append('<div class="mc-window-images"></div>');
       _this.append('<div class="mc-window-text"></div>');
       w = _this.find('.mc-window-images');
       t = _this.find('.mc-window-text');
       ws = _this.find('img');
-
       ws.each( function() {
         div = $(this).parent()
         w.append(this);
         text.push(div.html());
         div.remove();
       });
-
       ws.each( function(i) {
         t.append('<div class="mc-item"></div>');
         t.find('.mc-item').last().html(text[i]);
       })
-
       ts = t.find('.mc-item');
-
       _this.css({
         'overflow': 'hidden'
       });
-
       w.css({
         'width': 100*ws.length + '%',
         'transition': config.transition
       });
-
       t.css({
         'position': 'absolute',
         'width': 100*ws.length + '%',
         'transition': config.transition
       });
-
       ts.css({
         'position': 'relative',
         'float': 'left',
-        'min-height': '100%',
-        'min-width': 100/ws.length + '%'
+        'width': 100/ws.length/config.inStep + '%'
       });
-
       ws.css({
         'float': 'left',
-        'min-height': '100%',
-        'max-width': 100/ws.length + '%'
+        'width': 100/ws.length/config.inStep + '%'
       });
-
+      if (config.cover) {
+        ts.css({
+          'min-height': '100%'
+        });
+        ws.css({
+          'min-height': '100%'
+        });
+      };
     }(this);
-
     mcMakeDots = function(_this) {
       if ($(config.dots).is($)) {
         config.dots.append('<ul class="mc-ul"></ul>');
         u = config.dots.find('.mc-ul');
-        ws.each( function() {
+        for (var i=0; i<ws.length-(config.inStep-1); i++) {
           u.append('<li></li>');
-        });
+        }
       }
     }(this);
-
     function mcActiveDots() {
-      u.find('li.active').removeClass('active');
-      u.find('li').eq(config.currentNum).addClass('active');
+      if ($(config.dots).is($)) {
+        u.find('li.active').removeClass('active');
+        u.find('li').eq(config.currentNum).addClass('active');
+      }
     };
-
     slide = function(p) {
       config.currentNum += config.currentNum+p >= 0 && config.currentNum+p
-          < ws.length ? p : 0;
-      w.css('margin-left', -config.currentNum*100 + '%');
-      t.css('margin-left', -config.currentNum*100 + '%');
+          < ws.length-(config.inStep-1) ? p : 0;
+      w.css('margin-left', -config.currentNum*100/config.inStep + '%');
+      t.css('margin-left', -config.currentNum*100/config.inStep + '%');
       mcActiveDots();
     }
-
     mcActiveDots();
-    config.next.click( function() {
-      slide(1);
-    });
-    config.prev.click( function() {
-      slide(-1);
-    });
+    if  (config.next !== null) {
+      config.next.click( function() {
+        slide(1);
+      });
+    }
+    if  (config.prev !== null) {
+      config.prev.click( function() {
+        slide(-1);
+      });
+    }
   }
 })(jQuery);
