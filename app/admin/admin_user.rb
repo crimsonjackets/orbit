508b7ce4846ba356d61a83afd602250b9735d7e5
wyde19 <tiburon1995@gmail.com>
2014-10-09 17:24:15 +0400

    AdminUser crud finalized

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,19 +1,22 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
+  actions :all, except: :destroy
 
   index do
-    selectable_column
-    id_column
-    column :email
+    column :email do |admin|
+      link_to admin.email, oradmin_admin_user_path(admin)
+    end
     column :current_sign_in_at
+    column :current_sign_in_ip
+    column :last_sign_in_at
+    column :last_sign_in_ip
     column :sign_in_count
-    column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
+  filter :last_sign_in_at
+  filter :last_sign_in_ip
   filter :created_at
 
   form do |f|
@@ -23,6 +26,17 @@ ActiveAdmin.register AdminUser do
       f.input :password_confirmation
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :current_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_at
+      row :last_sign_in_ip
+      row :sign_in_count
+    end
   end
 
 end
