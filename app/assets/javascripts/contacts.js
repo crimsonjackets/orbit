d3baa636dd0b465a0ede9446861708792db7606f
Surmin Dmitry <surmin2005@yandex.ru>
2014-10-09 15:07:07 +0400

    jsfix

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
+    function initialize() {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+            address: "Санкт-Петербург, Левашовский пр., д. 12"
+        }, function (result) {
+            var mapOptions = {
+                    center: result[0].geometry.location,
+                    disableDefaultUI: true,
+                    zoom: 17,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
+                map = new google.maps.Map(document.getElementById(
+                    "map-canvas"), mapOptions),
+                marker = new google.maps.Marker({
+                    position: result[0].geometry.location,
+                    map: map
+                });
+        });
+    }
     if ($('body#contacts').length) {
-        function initialize() {
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({
-                address: "Санкт-Петербург, Левашовский пр., д. 12"
-            }, function (result) {
-                var mapOptions = {
-                        center: result[0].geometry.location,
-                        disableDefaultUI: true,
-                        zoom: 17,
-                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                    },
-                    map = new google.maps.Map(document.getElementById(
-                        "map-canvas"), mapOptions),
-                    marker = new google.maps.Marker({
-                        position: result[0].geometry.location,
-                        map: map
-                    });
-            });
-        }
         initialize();
     }
 });
