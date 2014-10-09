861dba26ce7ca05298d703264e8d3d9d51160ad1
wyde19 <tiburon1995@gmail.com>
2014-10-09 17:03:49 +0400

    Job elements initial

---
+++ db/migrate/20141009130130_create_jobs.rb
@@ -0,0 +1,8 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.text :description
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009125333) do
+ActiveRecord::Schema.define(version: 20141009130130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,11 @@ ActiveRecord::Schema.define(version: 20141009125333) do
   end
 
   create_table "home_mains", force: true do |t|
+    t.string "title"
+    t.text   "description"
+  end
+
+  create_table "jobs", force: true do |t|
     t.string "title"
     t.text   "description"
   end
