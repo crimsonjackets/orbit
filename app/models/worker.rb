13ec5bd257cdfa3d136e25e990bcd61ad08f303d
wyde19 <tiburon1995@gmail.com>
2014-10-09 17:45:39 +0400

    Add cropping styles to a worker

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,7 +1,9 @@
 class Worker < ActiveRecord::Base
   has_attached_file :attachment ,
-                    styles: {thumb: '192x108#', medium:'576x324#'},
-                    default_url: 'project_photos/:style/missing.png'
+                    styles: {thumb: '134x110#',
+                             x333:'268x333#',
+                             x220:'268x220#'},
+                    default_url: 'worker_photos/:style/missing.png'
   validates_attachment_content_type :attachment,
                                     content_type: /\Aimage\/.*\Z/
 end
